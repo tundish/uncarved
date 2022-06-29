ac94e7b2a920762bcd5f9f7b9a2abf526f446a4e
Dave Haynes <dave@thuswise.co.uk>
2022-06-29 17:21:59 +0100

    Add test for table method.

--- utils/toml2gml.py
+++ utils/toml2gml.py
@@ -65,7 +65,19 @@ class Model:
         return data
 
     def to_dot(self):
-        a = 1
+        print(self.data)
+        arcs = []
+        for name in self.tables:
+            parent = name.split(".")[-1]
+            p = self.table(parent)
+            t = self.table(name)
+            if parent != name:
+                if self.is_arc(t):
+                    pass
+                else:
+                    arcs.append(f"{p.labels[0]} -> {t.labels[0]}")
+
+        a = "\n".join(arcs)
         return dedent(f"""
         strict digraph {{
         {a}
@@ -87,6 +99,20 @@ class TestLoad(unittest.TestCase):
         """
         model = Model.loads(text)
         self.assertEqual(["A", "A.B.C", "D"], model.tables)
+
+    def test_table(self):
+        text = """
+        [A]
+        tag = 1
+
+        [A.B.C]
+        tag = 2
+
+        [  D ]
+        """
+        model = Model.loads(text)
+        self.assertEqual(1, model.table("A").get("tag"))
+        self.assertEqual({"tag": 2}, model.table("A.B.C"))
 
     def test_get(self):
         text = """
@@ -150,6 +176,7 @@ def main(args):
 
     model = Model.loads(text)
     print(model.tables)
+    print(model.to_dot())
 
 
 def parser():
